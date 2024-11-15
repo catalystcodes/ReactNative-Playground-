a385faa258743aa5a3d7a817f889498618e9a3a2
catalystcodes <muh4mub@gmail.com>
2024-11-15 17:06:27 +0100

    feat: simple blog with styled_component

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import styled from "styled-components";
-import { Styled } from "styled-components/native";
+import styled from "styled-components/native";
 import PressableButton from "./src/components/atoms/PressableButton";
 import Header from "./src/components/atoms/header";
 import Avatar from "./src/components/atoms/avatar";
+import InputContainer from "./src/components/atoms/inputContainer";
+import Card from "./src/components/atoms/card";
+import { DATA } from "./src/components/atoms/data";
 
 export default function App() {
   return (
@@ -12,7 +14,9 @@ export default function App() {
       <Header headerTitle="social" />
       <RowContainer>
         <Avatar imageSource={require("./src/assets/avatar1.png")} />
+        <InputContainer />
       </RowContainer>
+      <Card data={DATA} />
     </Container>
   );
 }
@@ -24,5 +28,4 @@ const Container = styled.SafeAreaView`
 const RowContainer = styled.View`
   width: 100%;
   padding-horizontal: 10px;
-  flex-direction: row;
 `;
---
+++ src/components/atoms/data.js
@@ -0,0 +1,34 @@
+export const DATA = [
+  {
+    id: "1",
+    userAvatar: require("../../../src/assets/avatar2.png"),
+    userName: "User 1",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    postImage: require("../../../src/assets/post1.png"),
+  },
+  {
+    id: "2",
+    userAvatar: require("../../../src/assets/avatar3.png"),
+    userName: "User 2",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    postImage: require("../../../src/assets/post2.png"),
+  },
+  {
+    id: "3",
+    userAvatar: require("../../../src/assets/avatar4.png"),
+    userName: "User 3",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    postImage: require("../../../src/assets/post3.png"),
+  },
+  {
+    id: "4",
+    userAvatar: require("../../../src/assets/avatar4.png"),
+    userName: "User 4",
+    postText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    postImage: require("../../../src/assets/post4.png"),
+  },
+];
